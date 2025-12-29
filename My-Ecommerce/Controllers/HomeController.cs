82cc9d5221d801ade8e86ad6481b64f8680adbfd
Kh91067 <khaledahmedwhyd1234@gmail.com>
2025-12-29 20:51:58 +0200

    Initial commit

---
+++ My-Ecommerce/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using My_Ecommerce.Models;
+
+namespace My_Ecommerce.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            SouqComContext db = new SouqComContext();
+            var cats = db.Categories.ToList();
+            return View(cats);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Cart()
+        {
+            return View();
+        }
+        public IActionResult Categories()
+        {
+            SouqComContext db = new SouqComContext();
+            var cats = db.Categories.ToList();
+            return View(cats);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ My-Ecommerce/Models/Cart.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace My_Ecommerce.Models;
+
+public partial class Cart
+{
+    public int Id { get; set; }
+
+    public string? UserId { get; set; }
+
+    public int? ProductId { get; set; }
+
+    public int? Quantity { get; set; }
+
+    public virtual Product? Product { get; set; }
+}
